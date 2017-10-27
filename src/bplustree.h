ab05678f4056f71ae39833fa5b0156fb385429ed
Zbigniew Romanowski <romz@wp.pl>
2017-10-27 11:39:29 +0200

    Renamed form BPlusTree to bplustree

--- src/bplustree.h
+++ src/bplustree.h
@@ -1,23 +1,5 @@
-//
-//  BPlusTree.hpp
-//  BPlusTree.2a
-//
-//  Created by Amittai Aviram on 6/12/16.
-//  Copyright © 2016 Amittai Aviram. All rights reserved.
-//
-//  Licensed under the Apache License, Version 2.0 (the "License");
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
-//
-//  http://www.apache.org/licenses/LICENSE-2.0
-//
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
-
-#pragma once
+#ifndef BPLUSTREE_BPLUSTREE_H
+#define BPLUSTREE_BPLUSTREE_H
 
 #include <tuple>
 #include <vector>
@@ -101,3 +83,6 @@ private:
     Node* fRoot;
     Printer fPrinter;
 };
+
+#endif
+
