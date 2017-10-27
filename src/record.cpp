49edcaee20426e4265e43dd528d8dd478400a72d
Zbigniew Romanowski <romz@wp.pl>
2017-10-27 20:41:36 +0200

    Record: formating

--- src/record.cpp
+++ src/record.cpp
@@ -3,17 +3,26 @@
 #include "definitions.h"
 #include "record.h"
 
+//
+//
+//
 Record::Record( ValueType value )
     : m_value( value )
 {
 
 }
 
+//
+//
+//
 ValueType Record::value() const
 {
     return m_value;
 }
 
+//
+//
+//
 std::string Record::toString() const
 {
     std::ostringstream oss;
